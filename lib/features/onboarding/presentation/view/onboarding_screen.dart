e3e60467904a5f9a5ae397b342596769c471f043
Norhan-127 <norhanhamza127@gmail.com>
2023-12-15 14:35:20 +0200

    make on boarding screens scroll by using PageView.builder()

--- lib/features/onboarding/presentation/view/onboarding_screen.dart
+++ lib/features/onboarding/presentation/view/onboarding_screen.dart
@@ -12,81 +12,114 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
-  List<Map<String,String>> onBoardingData=[
+  PageController _pageController = PageController();
+  List<Map<String, String>> onBoardingData = [
     {
       'title': AppText.donate,
-      'description': 'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.',
+      'description':
+          'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.',
       'image': AppImages.firstOnBoarding, // Replace with your image asset
     },
     {
       'title': AppText.clothsDonate,
-      'description': 'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.',
+      'description':
+          'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.',
       'image': AppImages.secOnBoarding, // Replace with your image asset
     },
     {
       'title': AppText.foodDonate,
-      'description': 'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.',
+      'description':
+          'هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.',
       'image': AppImages.thirdOnBoarding, // Replace with your image asset
     },
-
   ];
+
   @override
   Widget build(BuildContext context) {
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              flex: 4,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  SizedBox(height: MediaQuery.of(context).size.height * 0.1),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.23,
-                    child: AspectRatio(
-                      aspectRatio: 3.5 / 5.5,
-                      child: Image.asset(AppImages.firstOnBoarding),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                padding: EdgeInsets.only(top: 39, bottom: 80, right: 24, left: 24),
-                decoration: BoxDecoration(
-                    color: AppColors.mainColor,
-                    borderRadius: BorderRadiusDirectional.only(
-                        topStart: Radius.circular(30),
-                        topEnd: Radius.circular(30))),
-                child: Column(
-                  children: [
-                    Text(
-                      AppText.donate,
-                      style: AppStyles.textStyle20.copyWith(
-                          color: AppColors.white, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: 35,
-                    ),
-                    Center(
-                      child: Text(
-                        "هناك حقيقة مثبتة منذ زمن طويل وهي أن المحتوى المقروء لصفحة ما سيلهي القارئ عن التركيز على الشكل الخارجي للنص أو شكل توضع الفقرات في الصفحة التي يقرأها.",
-                        style: AppStyles.textStyle13.copyWith(color: AppColors.white),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+        body: PageView.builder(
+          itemBuilder: (context, index) {
+            return OnboardingScreen(
+              title: onBoardingData[index]['title']!,
+              description: onBoardingData[index]['description']!,
+              image: onBoardingData[index]['image']!,
+            );
+          },
+          controller: _pageController,
+          itemCount: onBoardingData.length,
         ),
       ),
+    );
+  }
+}
+
+class OnboardingScreen extends StatelessWidget {
+  final String title;
+  final String description;
+  final String image;
+
+  const OnboardingScreen({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          flex: 4,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.1),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.23,
+                child: AspectRatio(
+                  aspectRatio: 3.5 / 5.5,
+                  child: Image.asset(image),
+                ),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Container(
+            padding:
+                const EdgeInsets.only(top: 39, bottom: 80, right: 24, left: 24),
+            decoration: const BoxDecoration(
+                color: AppColors.mainColor,
+                borderRadius: BorderRadiusDirectional.only(
+                    topStart: Radius.circular(30),
+                    topEnd: Radius.circular(30))),
+            child: Column(
+              children: [
+                Text(
+                  title,
+                  style: AppStyles.textStyle20.copyWith(
+                      color: AppColors.white, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                Text(
+                  description,
+                  style: AppStyles.textStyle13.copyWith(color: AppColors.white),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 86,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
