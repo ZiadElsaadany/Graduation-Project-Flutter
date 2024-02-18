103c1df6ef81e3b8449df5b53e4d6a1c9210a098
Norhan-127 <norhanhamza127@gmail.com>
2024-02-18 21:25:55 +0200

    adding background image

--- lib/features/onboarding/presentation/view/onboarding_screen.dart
+++ lib/features/onboarding/presentation/view/onboarding_screen.dart
@@ -12,16 +12,15 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-       create: (c)=>OnBoardingCubit(),
+      create: (c) => OnBoardingCubit(),
       child: const Directionality(
         textDirection: TextDirection.rtl,
         child: Scaffold(
           backgroundColor: AppColors.white,
-          body:OnBoardingBody(),
+          body: OnBoardingBody(),
         ),
       ),
     );
@@ -36,42 +35,33 @@ class OnBoardingBody extends StatefulWidget {
 }
 
 class _OnBoardingBodyState extends State<OnBoardingBody> {
-
   @override
   void dispose() {
-    super.dispose();
-if(context.mounted) {
-  BlocProvider.of<OnBoardingCubit>(context).pageController.dispose();
-
-}
+    if (context.mounted) {
+      BlocProvider.of<OnBoardingCubit>(context).pageController.dispose();
+      super.dispose();
+    }
   }
+
   @override
   Widget build(BuildContext context) {
-    var onBoardingCubit=BlocProvider.of<OnBoardingCubit>(context);
+    var onBoardingCubit = BlocProvider.of<OnBoardingCubit>(context);
     return PageView.builder(
       itemBuilder: (context, index) {
         return OnBoardingViewBody(
-          title: onBoardingCubit
-              .onBoardingData[index]['title']!,
-          description: onBoardingCubit
-              .onBoardingData[index]['description']!,
-          image: BlocProvider
-              .of<OnBoardingCubit>(context)
-              .onBoardingData[index]['image']!,
+          title: onBoardingCubit.onBoardingData[index]['title']!,
+          description: onBoardingCubit.onBoardingData[index]['description']!,
+          image: BlocProvider.of<OnBoardingCubit>(context).onBoardingData[index]
+              ['image']!,
         );
       },
-      controller:
-      onBoardingCubit.pageController,
-      onPageChanged: (c){
+      controller: onBoardingCubit.pageController,
+      onPageChanged: (c) {
         onBoardingCubit.getCurrentPageViewIndex(c);
-
       },
-      itemCount: BlocProvider
-          .of<OnBoardingCubit>(context)
-          .onBoardingData
-          .length,
+      itemCount:
+          BlocProvider.of<OnBoardingCubit>(context).onBoardingData.length,
       // physics: const NeverScrollableScrollPhysics(),
-
     );
   }
 }
