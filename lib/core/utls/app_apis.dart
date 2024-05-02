2dd97ff3e1156808da41e6cd764076e0a167da55
salsabilelshiekh2022 <salsabilelshiekh6@gmail.com>
2024-05-03 02:11:00 +0300

    -Get data in home

--- lib/core/utls/app_apis.dart
+++ lib/core/utls/app_apis.dart
@@ -3,4 +3,6 @@ class AppApis {
   static const String registerEndPoint = "/api/v1/auth/user/register";
   static const String getPostsEndPoint = "/api/v1/post";
   static const String likeEndPoint = "/api/v1/like";
+  static const String charityEndPoint = '/api/v1/charity';
+  static const String homeCampainsEndPoint = '/api/v1/campaign/1';
 }
--- lib/features/charity/presentation/view/widgets/charity_donation_value_container.dart
+++ lib/features/charity/presentation/view/widgets/charity_donation_value_container.dart
@@ -1,9 +1,8 @@
 import 'package:aoun_tu/core/utls/colors.dart';
-import 'package:aoun_tu/features/charity/presentation/view/view_model/charity_donation_values_cubit.dart';
+import 'package:aoun_tu/features/charity/presentation/view_model/charity_donation_values_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../../core/utls/styles.dart';
-
 
 class CharityDonationValuesContainer extends StatelessWidget {
   const CharityDonationValuesContainer({
@@ -21,13 +20,13 @@ class CharityDonationValuesContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<CharityDonationValuesCubit, CharityDonationValuesState>(
       builder: (context, state) {
-        var cubit=BlocProvider.of<CharityDonationValuesCubit>(context);
+        var cubit = BlocProvider.of<CharityDonationValuesCubit>(context);
         return GestureDetector(
           onTap: onTap,
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 20),
             decoration: BoxDecoration(
-              color: (cubit.charityItemIndex== index && cubit.selected == true)
+              color: (cubit.charityItemIndex == index && cubit.selected == true)
                   ? AppColors.mainColor
                   : Colors.transparent,
               borderRadius: BorderRadius.circular(12),
