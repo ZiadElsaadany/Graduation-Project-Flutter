77c4762153a7ef5987d37c199b2235ff8869e512
Norhan-127 <norhanhamza127@gmail.com>
2024-05-10 14:05:20 +0300

    update gift category model

--- lib/features/gift_donation/data/models/gift_category_model.dart
+++ lib/features/gift_donation/data/models/gift_category_model.dart
@@ -1,11 +1,30 @@
+import 'dart:convert';
 import 'dart:ui';
 
 class GiftCategoryModel {
-  final String? title;
   final String? image;
   final Color? color;
+  final String? message;
+  final List<String>? categories;
+  final int? statusCode;
+  final bool? isSuccessful;
 
-  GiftCategoryModel(this.title, this.image, this.color);
+  GiftCategoryModel(
+      {this.image,
+      this.color,
+      this.message,
+      this.categories,
+      this.statusCode,
+      this.isSuccessful});
+
+  factory GiftCategoryModel.fromJson(String jsonString) {
+    final Map<String, dynamic> json = jsonDecode(jsonString);
+    return GiftCategoryModel(
+      message: json['message'] as String,
+      categories:
+          (json['data'] as List<String>).toList(),
+      statusCode: json['statusCode'] as int,
+      isSuccessful: json['isSuccessful'] as bool,
+    );
+  }
 }
-
-
