e7046809b215d3ce5edc3b71c4332fff1e2d8afa
Norhan-127 <norhanhamza127@gmail.com>
2024-05-10 14:10:28 +0300

    adding gift category repo

---
+++ lib/features/gift_donation/data/repos/gift_donation_repo.dart
@@ -0,0 +1,8 @@
+import 'package:aoun_tu/features/gift_donation/data/models/gift_category_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+
+abstract class GiftDonationRepo{
+  Future<Either<Failure,GiftCategoryModel>> getGiftCategoriesData();
+}
