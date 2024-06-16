abd869f6779137cbea0b09b0ced4fc994b7e04eb
Norhan-127 <norhanhamza127@gmail.com>
2024-06-17 01:07:44 +0300

    push credit card screen from GiftDataDetailsViewBody

--- lib/features/gift_donation/presentation/view/widget/gift_data_details_view_body.dart
+++ lib/features/gift_donation/presentation/view/widget/gift_data_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:aoun_tu/core/utls/routers.dart';
 import 'package:aoun_tu/core/utls/styles.dart';
 import 'package:aoun_tu/core/utls/text.dart';
 import 'package:aoun_tu/features/gift_donation/presentation/view/widget/confirm_code_custom_button.dart';
@@ -9,27 +10,30 @@ import 'package:aoun_tu/features/gift_donation/presentation/view_model/gift_cubi
 import 'package:aoun_tu/features/home/presentation/view/widgets/app_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import '../../../../../core/utls/colors.dart';
 
 class GiftDataDetailsViewBody extends StatefulWidget {
   const GiftDataDetailsViewBody({Key? key}) : super(key: key);
 
   @override
-  State<GiftDataDetailsViewBody> createState() => _GiftDataDetailsViewBodyState();
+  State<GiftDataDetailsViewBody> createState() =>
+      _GiftDataDetailsViewBodyState();
 }
 
 class _GiftDataDetailsViewBodyState extends State<GiftDataDetailsViewBody> {
-  final TextEditingController recipientNameTextController = TextEditingController();
-  final TextEditingController recipientPhoneTextController = TextEditingController();
-  final TextEditingController senderPhoneTextController = TextEditingController();
-
+  final TextEditingController recipientNameTextController =
+      TextEditingController();
+  final TextEditingController recipientPhoneTextController =
+      TextEditingController();
+  final TextEditingController senderPhoneTextController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(
-          horizontal: 24,
-          vertical: MediaQuery.of(context).size.height * 0.03),
+          horizontal: 24, vertical: MediaQuery.of(context).size.height * 0.03),
       child: ListView(
         children: [
           const GiftDataDetailsHeaderText(
@@ -52,8 +56,7 @@ class _GiftDataDetailsViewBodyState extends State<GiftDataDetailsViewBody> {
                 GiftDataSRTextFormField(
                   hintText: AppText.enterRecipientPhone,
                   labelText: AppText.recipientPhone,
-                  controller:recipientPhoneTextController,
-
+                  controller: recipientPhoneTextController,
                 ),
                 const SizedBox(
                   height: 24,
@@ -79,15 +82,14 @@ class _GiftDataDetailsViewBodyState extends State<GiftDataDetailsViewBody> {
                 GiftDataSRTextFormField(
                   hintText: AppText.enterSenderPhone,
                   labelText: AppText.confirmSenderPhone,
-                  controller:senderPhoneTextController,
+                  controller: senderPhoneTextController,
                 ),
                 const SizedBox(
                   height: 8,
                 ),
                 Text(
                   AppText.thisPhoneWillBeSentToTheRecipient,
-                  style:
-                      AppStyles.textStyle12.copyWith(color: AppColors.text1),
+                  style: AppStyles.textStyle12.copyWith(color: AppColors.text1),
                 ),
                 SizedBox(
                   height: MediaQuery.of(context).size.height * 0.034,
@@ -96,11 +98,8 @@ class _GiftDataDetailsViewBodyState extends State<GiftDataDetailsViewBody> {
                   builder: (context, state) {
                     return ConfirmCodeCustomButton(
                       title: AppText.phoneConfirm,
-                      color:
-                           AppColors.yellow,
-                      onTap: () {
-
-                      },
+                      color: AppColors.yellow,
+                      onTap: () {},
                     );
                   },
                 ),
@@ -113,6 +112,8 @@ class _GiftDataDetailsViewBodyState extends State<GiftDataDetailsViewBody> {
                       AppStyles.textStyle17.copyWith(color: AppColors.white),
                   color: AppColors.frame,
                   height: MediaQuery.of(context).size.height * 0.055,
+                  onTap: () =>
+                      GoRouter.of(context).push(AppRouter.kCreditCardScreen),
                 ),
               ],
             ),
